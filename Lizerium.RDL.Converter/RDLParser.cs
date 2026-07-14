cabfaa2bbf275d7662bf00a2424deac631884e37
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-14 06:53:26 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-14

--- Lizerium.RDL.Converter/RDLParser.cs
+++ Lizerium.RDL.Converter/RDLParser.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky, Jason Hood (adoxa)
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 июля 2026 06:52:53
- * Version: 1.0.91
+ * Last Updated: 14 июля 2026 06:53:24
+ * Version: 1.0.92
  */
 
 using System.Text;
