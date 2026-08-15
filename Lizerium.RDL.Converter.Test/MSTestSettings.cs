844e2ebd891851b2c0f7bc178fd0b9d9a58bb315
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-15 06:53:09 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-15

--- Lizerium.RDL.Converter.Test/MSTestSettings.cs
+++ Lizerium.RDL.Converter.Test/MSTestSettings.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky, Jason Hood (adoxa)
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 августа 2026 08:17:07
- * Version: 1.0.123
+ * Last Updated: 15 августа 2026 06:53:07
+ * Version: 1.0.124
  */
 
 [assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
