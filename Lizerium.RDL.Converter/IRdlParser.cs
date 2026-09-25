ee042cc8b263b705a7c3ec3da4688a0c1daf2c25
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-25 09:35:50 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-25

--- Lizerium.RDL.Converter/IRdlParser.cs
+++ Lizerium.RDL.Converter/IRdlParser.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky, Jason Hood (adoxa)
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 сентября 2026 09:37:33
- * Version: 1.0.164
+ * Last Updated: 25 сентября 2026 09:35:49
+ * Version: 1.0.165
  */
 
 using System;
